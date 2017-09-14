a39343d570dd951aa82e9650ed00cdb671e81d15
dmcarthur5 <david.mca94@googlemail.com>
2017-09-14 14:36:18 +0100

    AI decisions complete

--- src/Turn.java
+++ src/Turn.java
@@ -48,14 +48,54 @@ public class Turn {
 
     //Find highest value card
     private Card DecideFirstCard(){
-        return null;
+        Card decision = null;
+        int maxValue = 0;
+        for(Card card : this.currentPlayer.Hand){
+            if (card.getIntValue() > maxValue){
+                maxValue = card.getIntValue();
+                decision = card;
+            }
+        }
+        return decision;
     }
 
     //Lowest winning heart
     //Lowest losing heart
     //Lowest card
     private Card DecideHeartsCard(){
-        return null;
+        Card winningHeart = null;
+        Card losingHeart = null;
+        Card lowestCard = null;
+        int minWinningHeartValue = 15;
+        int minLosingHeartValue = 15;
+        int minCardValue = 15;
+        for (Card card : this.currentPlayer.Hand){
+            if(card.getSuit() == Suit.HEARTS){
+                if(card.getIntValue() > this.currentWinner.getIntValue()){
+                    if(card.getIntValue() < minWinningHeartValue){
+                        minCardValue = card.getIntValue();
+                        winningHeart = card;
+                    }
+                }
+                else{
+                    if(card.getIntValue() < minLosingHeartValue){
+                        minLosingHeartValue = card.getIntValue();
+                        losingHeart = card;
+                    }
+                }
+            }
+            if(card.getIntValue() < minCardValue){
+                minCardValue = card.getIntValue();
+                lowestCard = card;
+            }
+        }
+        if(winningHeart != null){
+            return winningHeart;
+        }
+        if(losingHeart != null){
+            return losingHeart;
+        }
+        return lowestCard;
     }
 
     //Lowest winning suit
@@ -63,7 +103,50 @@ public class Turn {
     //Lowest heart
     //Lowest card
     private Card DecideNonHeartsCard(){
-        return null;
+        Card winningSuit = null;
+        Card losingSuit = null;
+        Card lowestHeart = null;
+        Card lowestCard = null;
+        int minWinningSuitValue = 15;
+        int minLosingSuitValue = 15;
+        int minHeartValue = 15;
+        int minCardValue = 15;
+        for(Card card : this.currentPlayer.Hand){
+            if(card.getSuit() == this.roundSuit){
+                if(card.getIntValue() > this.currentWinner.getIntValue()){
+                    if(card.getIntValue() < minWinningSuitValue){
+                        minWinningSuitValue = card.getIntValue();
+                        winningSuit = card;
+                    }
+                }
+                else{
+                    if(card.getIntValue() < minLosingSuitValue){
+                        minLosingSuitValue = card.getIntValue();
+                        losingSuit = card;
+                    }
+                }
+            }
+            if(card.getSuit() == Suit.HEARTS){
+                if(card.getIntValue() < minHeartValue){
+                    minHeartValue = card.getIntValue();
+                    lowestHeart = card;
+                }
+            }
+            if(card.getIntValue() < minCardValue){
+                minCardValue = card.getIntValue();
+                lowestCard = card;
+            }
+        }
+        if(winningSuit != null){
+            return winningSuit;
+        }
+        if(losingSuit != null){
+            return losingSuit;
+        }
+        if(lowestHeart != null){
+            return lowestHeart;
+        }
+        return lowestCard;
     }
 
     private Card CurrentWinner(){
